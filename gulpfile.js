aead36bdd96682af754e27d6e68953d1fc56dfe1
lwei <liangwei>
2017-05-06 18:42:23 +0800

    save

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var fileinclude  = require('gulp-file-include');
 var htmlminify = require("gulp-html-minify");
+var less = require('gulp-less');
 
 gulp.task('fileinclude', function() {
 	gulp.src('src/**/*.html')
@@ -15,10 +16,24 @@ gulp.task('fileinclude', function() {
 		.pipe(gulp.dest(''));
 });
 
+gulp.task('less', function() {
+	return gulp.src('**/*.less') // only compile the entry file
+		.pipe(less()).on('error', function(err) {
+			this.emit('end');
+			console.log(err);
+		})
+		.pipe(gulp.dest(''))
+});
+
 gulp.task('watchHtml', function() {
 	gulp.watch('src/**/*.html', function(){
 		gulp.run('fileinclude');
 	}); // Watch all the .less files, then run the less task
 });
+gulp.task('watchLess', function() {
+	gulp.watch('**/*.less', function(){
+		gulp.run('less');
+	}); // Watch all the .less files, then run the less task
+});
 
-gulp.task('default', ['fileinclude', 'watchHtml']);
+gulp.task('default', ['fileinclude', 'watchHtml', 'less', "watchLess"]);
